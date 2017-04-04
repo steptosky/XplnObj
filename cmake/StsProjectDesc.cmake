b4fdd96e667f5b1600041f0351b960d913420c43
Alex <pancir3d@steptosky.com>
2017-04-04 13:43:13 +0300

    Fixed description fro the conanfile and StsProjectDesc

--- cmake/StsProjectDesc.cmake
+++ cmake/StsProjectDesc.cmake
@@ -72,7 +72,7 @@ set(ProjectOrganizationWebLink "www.steptosky.com")
 
 set(ProjectName "X-Plane Obj Library")
 set(ProjectShortName "XplnObj")
-set(ProjectDescription "This library is for working with x-plane's obj format.")
+set(ProjectDescription "This library is for working with the x-plane's obj format.")
 set(ProjectWebLink "www.steptosky.com")
 set(ProjectSourcesWebLink "https://github.com/steptosky/XplnObj")
 

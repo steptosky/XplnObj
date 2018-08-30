a633413dca9854df9543cca5faacd17b0558ac40
Alexander Khudekov <pancir3d@steptosky.com>
2018-08-30 18:58:43 +0300

    Updated data according to the release

--- cmake/StsProjectDesc.cmake
+++ cmake/StsProjectDesc.cmake
@@ -87,7 +87,7 @@ set(ProjectId ${ProjectName})
 # Set it to ON if you are in unstable branch otherwise set it to OFF
 set(ProjectVersionSnapshot OFF)
 set(ProjectVersionMajor 0)
-set(ProjectVersionMinor 5)
+set(ProjectVersionMinor 6)
 set(ProjectVersionPatch 0)
 set(ProjectVersion "${ProjectVersionMajor}.${ProjectVersionMinor}.${ProjectVersionPatch}")
 

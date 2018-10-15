35e57b45c6ecf6d6a514c1a54067128ada63e1fc
Alexander Khudekov <pancir3d@steptosky.com>
2018-10-15 15:41:09 +0300

    Added datarefs file reader

---
+++ src-test/utils/TestDatarefsFile.cpp
@@ -0,0 +1,197 @@
+/*
+**  Copyright(C) 2018, StepToSky
+**
+**  Redistribution and use in source and binary forms, with or without
+**  modification, are permitted provided that the following conditions are met:
+**
+**  1.Redistributions of source code must retain the above copyright notice, this
+**    list of conditions and the following disclaimer.
+**  2.Redistributions in binary form must reproduce the above copyright notice,
+**    this list of conditions and the following disclaimer in the documentation
+**    and / or other materials provided with the distribution.
+**  3.Neither the name of StepToSky nor the names of its contributors
+**    may be used to endorse or promote products derived from this software
+**    without specific prior written permission.
+**
+**  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+**  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+**  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+**  DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+**  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+**  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+**  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+**  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+**  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+**  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+**  Contacts: www.steptosky.com
+*/
+
+#include <gtest/gtest.h>
+#include <xpln/utils/DatarefsFile.h>
+#include <sstream>
+
+using namespace xobj;
+using namespace std::string_literals;
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+void equals(const Dataref & drf1, const Dataref & drf2) {
+    ASSERT_TRUE(drf1.mId == drf2.mId);
+    ASSERT_TRUE(drf1.mKey == drf2.mKey);
+    ASSERT_TRUE(drf1.mWritable == drf2.mWritable);
+    ASSERT_TRUE(drf1.mValueType == drf2.mValueType);
+    ASSERT_TRUE(drf1.mValueUnits == drf2.mValueUnits);
+    ASSERT_TRUE(drf1.mDescription == drf2.mDescription);
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+TEST(DatarefsFile, read_normal_drf) {
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    std::stringstream stream;
+    stream << " first line " << std::endl;
+    stream << std::endl;
+    stream << R"(sim/aircraft/autopilot/vvi_step_ft	float	y	Feet	Step increment for autopilot VVI)" << std::endl;
+    stream << R"(sim/aircraft/engine/acf_max_OILP	float	y	???	Max Oil.)" << std::endl;
+    stream << R"(sim/aircraft/view/acf_Vno	float	y)" << std::endl;
+    stream << R"(only_key_test)" << std::endl;
+    //-----------------------
+    DatarefsFile drf;
+    ASSERT_NO_THROW(drf.loadStream(stream));
+    ASSERT_EQ(4, drf.mData.size());
+
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{true, uint64Max, "sim/aircraft/autopilot/vvi_step_ft", "float", "Feet", "Step increment for autopilot VVI"},
+        drf.mData[0]));
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{true, uint64Max, "sim/aircraft/engine/acf_max_OILP", "float", "???", "Max Oil."},
+        drf.mData[1]));
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{true, uint64Max, "sim/aircraft/view/acf_Vno", "float", "", ""},
+        drf.mData[2]));
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{false, uint64Max, "only_key_test", "", "", "" },
+        drf.mData[3]));
+}
+
+TEST(DatarefsFile, read_normal_drf_custom) {
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    std::stringstream stream;
+    stream << " first line " << std::endl;
+    stream << std::endl;
+    stream << R"(000001	sim/aircraft/autopilot/vvi_step_ft	float	y	Feet	Step increment for autopilot VVI)" << std::endl;
+    stream << R"(000005	sim/aircraft/engine/acf_max_OILP	float	y	???	Max Oil.)" << std::endl;
+    stream << R"(sim/aircraft/view/acf_Vno	float	y)" << std::endl;
+    stream << R"(000002	only_key_test_1)" << std::endl;
+    stream << R"(#000008	only_key_test_2)" << std::endl;
+    //-----------------------
+    DatarefsFile drf;
+    ASSERT_NO_THROW(drf.loadStream(stream));
+    ASSERT_EQ(4, drf.mData.size());
+    ASSERT_EQ(6, drf.generateId());
+
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{true, 1, "sim/aircraft/autopilot/vvi_step_ft", "float", "Feet", "Step increment for autopilot VVI" },
+        drf.mData[0]));
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{true, 5, "sim/aircraft/engine/acf_max_OILP", "float", "???", "Max Oil." },
+        drf.mData[1]));
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{true, uint64Max, "sim/aircraft/view/acf_Vno", "float", "", "" },
+        drf.mData[2]));
+    ASSERT_NO_FATAL_FAILURE(equals(Dataref{false, 2, "only_key_test_1", "", "", "" },
+        drf.mData[3]));
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+TEST(DatarefsFile, write_normal_drf_custom) {
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    //-----------------------
+    DatarefsFile drf;
+    drf.mData = {
+        Dataref{true, uint64Max, "sim/aircraft/autopilot/vvi_step_ft", "float", "Feet", "Step increment for autopilot VVI"},
+        Dataref{false, 3, "sim/aircraft/engine/acf_max_OILP", "float", "???", "Max Oil."},
+        Dataref{true, 4, "sim/aircraft/view/acf_Vno", "float", "", ""},
+        Dataref{false, uint64Max, "only_key_test_1", "", "", ""},
+    };
+    //-----------------------
+    std::stringstream resultStream;
+    resultStream << std::endl;
+    resultStream << R"(sim/aircraft/autopilot/vvi_step_ft	float	y	Feet	Step increment for autopilot VVI)" << std::endl;
+    resultStream << R"(000003	sim/aircraft/engine/acf_max_OILP	float	n	???	Max Oil.)" << std::endl;
+    resultStream << R"(000004	sim/aircraft/view/acf_Vno	float	y)" << std::endl;
+    resultStream << R"(only_key_test_1)" << std::endl;
+    //-----------------------
+    std::stringstream stream;
+    drf.saveStream(stream);
+    ASSERT_STREQ(resultStream.str().c_str(), stream.str().c_str());
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+TEST(DatarefsFile, searchIdDuplicate_case1) {
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    //-----------------------
+    DatarefsFile drf;
+    drf.mData = {
+        Dataref{false, uint64Max, "only_key_test_1", "", "", ""},
+        Dataref{false, 1, "only_key_test_2", "", "", ""},
+        Dataref{false, 1, "only_key_test_3", "", "", ""},
+        Dataref{false, 2, "only_key_test_3", "", "", ""},
+    };
+    //-----------------------
+    ASSERT_THROW(drf.searchIdDuplicate(), std::domain_error);
+}
+
+TEST(DatarefsFile, searchIdDuplicate_case2) {
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    //-----------------------
+    DatarefsFile drf;
+    drf.mData = {
+        Dataref{false, 1, "only_key_test_1", "", "", ""},
+        Dataref{false, 3, "only_key_test_2", "", "", ""},
+        Dataref{false, 1, "only_key_test_3", "", "", ""},
+        Dataref{false, 2, "only_key_test_3", "", "", ""},
+    };
+    //-----------------------
+    ASSERT_THROW(drf.searchIdDuplicate(), std::domain_error);
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+TEST(DatarefsFile, searchKeyDuplicate_case1) {
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    //-----------------------
+    DatarefsFile drf;
+    drf.mData = {
+        Dataref{false, uint64Max, "only_key_test_1", "", "", ""},
+        Dataref{false, uint64Max, "only_key_test_2", "", "", ""},
+        Dataref{false, uint64Max, "only_key_test_2", "", "", ""},
+        Dataref{false, uint64Max, "only_key_test_3", "", "", ""},
+    };
+    //-----------------------
+    ASSERT_THROW(drf.searchKeyDuplicate(), std::domain_error);
+}
+
+TEST(DatarefsFile, searchKeyDuplicate_case2) {
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    //-----------------------
+    DatarefsFile drf;
+    drf.mData = {
+        Dataref{false, uint64Max, "only_key_test_3", "", "", ""},
+        Dataref{false, uint64Max, "only_key_test_2", "", "", ""},
+        Dataref{false, uint64Max, "only_key_test_3", "", "", ""},
+        Dataref{false, uint64Max, "only_key_test_3", "", "", ""},
+    };
+    //-----------------------
+    ASSERT_THROW(drf.searchKeyDuplicate(), std::domain_error);
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
---
+++ src/utils/DatarefsFile.cpp
@@ -0,0 +1,191 @@
+/*
+**  Copyright(C) 2018, StepToSky
+**
+**  Redistribution and use in source and binary forms, with or without
+**  modification, are permitted provided that the following conditions are met:
+**
+**  1.Redistributions of source code must retain the above copyright notice, this
+**    list of conditions and the following disclaimer.
+**  2.Redistributions in binary form must reproduce the above copyright notice,
+**    this list of conditions and the following disclaimer in the documentation
+**    and / or other materials provided with the distribution.
+**  3.Neither the name of StepToSky nor the names of its contributors
+**    may be used to endorse or promote products derived from this software
+**    without specific prior written permission.
+**
+**  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+**  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+**  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+**  DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+**  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+**  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+**  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+**  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+**  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+**  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+**  Contacts: www.steptosky.com
+*/
+
+#include "xpln/utils/DatarefsFile.h"
+#include <fstream>
+#include <iomanip>
+#include <cctype>
+
+namespace xobj {
+
+/**************************************************************************************************/
+//////////////////////////////////////////* Functions */////////////////////////////////////////////
+/**************************************************************************************************/
+
+void DatarefsFile::loadFile(const std::string & filePath) {
+    using namespace std::string_literals;
+    std::ifstream file(filePath, std::iostream::in);
+    if (!file) {
+        throw std::runtime_error(ExcTxt("can't open file <"s.append(filePath).append(">")));
+    }
+    try {
+        loadStream(file);
+        file.close();
+    }
+    catch (...) {
+        file.close();
+        throw;
+    }
+}
+
+void DatarefsFile::loadStream(std::istream & input) {
+    const char * sep = "\t";
+    mData.clear();
+    mLastId = 0;
+    std::string line;
+    if (!std::getline(input, line)) {
+        return;
+    }
+    while (std::getline(input, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        if (line.front() == '#') {
+            continue;
+        }
+        Dataref drf;
+        const auto values = sts::MbStrUtils::splitToVector(line, sep);
+        if (values.empty()) {
+            continue;
+        }
+
+        std::size_t position = 0;
+        if (std::isdigit(values[position].front())) {
+            drf.mId = std::stoul(values[position++]);
+            mLastId = std::max(mLastId, drf.mId);
+        }
+
+        if (position != values.size()) {
+            drf.mKey = values[position++];
+        }
+        if (position != values.size()) {
+            drf.mValueType = values[position++];
+        }
+        if (position != values.size()) {
+            drf.mWritable = values[position++].front() == 'y';
+        }
+        if (position != values.size()) {
+            drf.mValueUnits = values[position++];
+        }
+        if (position != values.size()) {
+            drf.mDescription = values[position];
+        }
+
+        mData.emplace_back(std::move(drf));
+    }
+}
+
+/**************************************************************************************************/
+//////////////////////////////////////////* Functions */////////////////////////////////////////////
+/**************************************************************************************************/
+
+void DatarefsFile::saveFile(const std::string & filePath) {
+    using namespace std::string_literals;
+    std::ofstream file(filePath, std::iostream::out);
+    if (!file) {
+        throw std::runtime_error(ExcTxt("can't open file <"s.append(filePath).append(">")));
+    }
+    try {
+        saveStream(file);
+        file.close();
+    }
+    catch (...) {
+        file.close();
+        throw;
+    }
+}
+
+void DatarefsFile::saveStream(std::ostream & output) {
+    const char * sep = "\t";
+    output << std::endl;
+    for (auto & drf : mData) {
+        if (drf.mId != std::numeric_limits<std::uint64_t>::max()) {
+            output << std::setfill('0') << std::setw(6) << drf.mId << sep;
+        }
+        output << drf.mKey;
+        if (!drf.mValueType.empty()) {
+            output << sep << drf.mValueType;
+            output << sep << (drf.mWritable ? 'y' : 'n');
+            if (!drf.mValueUnits.empty()) {
+                output << sep << drf.mValueUnits;
+                if (!drf.mDescription.empty()) {
+                    output << sep << drf.mDescription;
+                }
+            }
+        }
+        output << std::endl;
+    }
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+std::uint64_t DatarefsFile::generateId() {
+    mLastId += 1;
+    return mLastId;
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+void DatarefsFile::searchIdDuplicate() {
+    using namespace std::string_literals;
+    const auto uint64Max = std::numeric_limits<std::uint64_t>::max();
+    for (auto iter = mData.begin(); iter != mData.end(); ++iter) {
+        auto findIter = std::find_if(iter, mData.end(), [&](const auto & val) {
+            if (iter->mId == uint64Max || val.mId == uint64Max) {
+                return false;
+            }
+            return iter->mId == val.mId;
+        });
+        if (findIter != mData.end()) {
+            throw std::domain_error(ExcTxt("found id duplicate <"s.append(std::to_string(iter->mId)).append(">")));
+        }
+    }
+}
+
+void DatarefsFile::searchKeyDuplicate() {
+    using namespace std::string_literals;
+    for (auto iter = mData.begin(); iter != mData.end(); ++iter) {
+        auto findIter = std::find_if(iter, mData.end(), [&](const auto & val) {
+            return iter->mKey == val.mKey;
+        });
+        if (findIter != mData.end()) {
+            throw std::domain_error(ExcTxt("found key duplicate <"s.append(iter->mKey).append(">")));
+        }
+    }
+}
+
+/**************************************************************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************/
+
+}
